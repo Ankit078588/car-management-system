b4d522893c077dbefc91e35b5b5c9d3d2d333a58
Ankit <ankit@Ankits-MacBook-Air.local>
2024-11-16 09:20:15 +0530

    Done

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,14 +1,8 @@
-const cloudinary = require("cloudinary").v2
+// const cloudinary = require("cloudinary").v2
 
-exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
-  const options = { folder }
-  if (height) {
-    options.height = height
-  }
-  if (quality) {
-    options.quality = quality
-  }
-  options.resource_type = "auto"
-  console.log("OPTIONS", options)
-  return await cloudinary.uploader.upload(file.tempFilePath, options)
-}
+// Configure Cloudinary
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// });
